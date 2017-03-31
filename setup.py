005638ba393f36be593406aa0cb648826f771bba
Bartosz Kościów <kosci1@gmail.com>
2017-03-31 09:55:06 +0200

    doc

--- setup.py
+++ setup.py
@@ -8,4 +8,16 @@ setup(
 	description="Dockerfile.dist to Dockerfile. in progress",
     scripts=['bin/docker-compile'],
 	author="Bartosz Kosciow",
+	classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
 )
